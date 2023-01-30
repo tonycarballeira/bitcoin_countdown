ce232022a1927fe6c74a2e3cd889c6072030015b
tonycarballeira <tonycarballeira@gmail.com>
2023-01-30 10:59:50 -0500

    parsing json object from get request then calculating seconds left until bitcoin halving

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const request = new XMLHttpRequest();
 request.open("GET","https://blockchain.info/stats?format=json");
 request.send();
+
+var jsonObject = {};
+
+var secondsLeft = 0;
+
 request.onload = function(){
-    console.log(request.response);
+    jsonObject = JSON.parse(request.response);
+    const blocksLeft = jsonObject.n_blocks_total - 420000;
+    const minsBetween = jsonObject.minutes_between_blocks;
+    const minsLeft = blocksLeft * minsBetween;
+    secondsLeft = minsLeft * 60;
 }
+
+
+function callIt(){
+    secondsLeft = secondsLeft - 1;
+    console.log(secondsLeft);
+}
+
+callIt();
+
+setInterval(callIt, 1000);
+
+
+function clicker() {
+    console.log(jsonObject);
+}
+
+//NOTES
+
+//When receiving data from a web server, the data is always a string.
+//Parse the data with JSON.parse(), and the data becomes a JavaScript object.
